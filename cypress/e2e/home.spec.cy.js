7d726792827c80006c69a0c44578653f459438e3
Bruno Almeida <bruno4893@hotmail.com>
2022-10-13 16:20:55 -0300

    Testes com cypress 1.0

---
+++ cypress/e2e/home.spec.cy.js
@@ -0,0 +1,9 @@
+
+
+describe('home page', ()=>{
+    it('App deve estar online', ()=>{ // Teste para saber se o site/app tá online
+        cy.viewport(1440, 900) // Muda a resolução do site pra uma resolução mais condizente com a nossa tela
+        cy.visit('https://buger-eats-qa.vercel.app') // Indica o caminho do site que vamos testar.
+        cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats') // Faz uma verificação para saber se o site está online.
+    })
+})
