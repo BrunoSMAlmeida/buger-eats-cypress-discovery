a56de2d2db314a595010a90570908531df0b0085
Bruno Almeida <bruno4893@hotmail.com>
2022-10-13 16:50:48 -0300

    Informações do dashboard do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+      projectId: "m1tvxw",
       "viewportWidth": 1440,
       "viewportHeight": 900,
       "baseUrl": "https://buger-eats-qa.vercel.app/",
